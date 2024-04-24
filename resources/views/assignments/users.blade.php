2a00c6b106ca69fde4faad6a9e7b465f893b3373
Petedarkpete <peterndahi2018@gmail.com>
2024-04-24 12:17:29 +0300

    users table

--- resources/views/assignments/users.blade.php
+++ resources/views/assignments/users.blade.php
@@ -1,6 +1,54 @@
 @extends('layouts.page')
 
 @section('content')
+<main id="main" class="main">
+    <div class="container">
+        <div class="row text-center pt-3">
+            <div class="col-lg-12">
+            <div class="pagetitle">
+                <h1>Users</h1>
+            </div><!-- End Page Title -->
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="card">
+        <div class="card-title d-flex justify-content-end">
+            <div class="col-12">
+                <button type="button" class="btn btn-primary btn-sm">Add User</button>
+            </div>
+        </div>
 
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered bg-info table-sm" id="users_table" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Adm No</th>
+                                <th>Year</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>tt</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>  
+</main>
 
 @endsection
